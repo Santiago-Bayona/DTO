0f2cc5c38db036fd706b563750a8f34f2a058891
Santiago-Bayona <Cesars.bayonap@uqvirtual.edu.co>
2025-03-30 21:30:36 -0500

    Update ExamenMedico.java

--- DTO_Record/src/main/java/co/edu/uniquindio/poo/dto_record/Modelo/ExamenMedico.java
+++ DTO_Record/src/main/java/co/edu/uniquindio/poo/dto_record/Modelo/ExamenMedico.java
@@ -1,18 +1,17 @@
 package co.edu.uniquindio.poo.dto_record.Modelo;
 
 public record ExamenMedico(JugadorDTO persona, double peso, double altura, boolean diabetes, String CodigoExamen ) {
-    public double CalcularIMC(){
-        double resultado=0.0;
-        resultado = peso/(altura*altura);
+    public String calcularIMC() {
+        double resultado = peso / (altura * altura);
+
         if (resultado < 18.5) {
-            System.out.println("Usted tiene un peso bajo según su IMC: " + resultado);
+            return "Bajo peso";
         } else if (resultado >= 18.5 && resultado < 24.9) {
-            System.out.println("Usted tiene un peso normal según su IMC: " + resultado);
+            return "Normal";
         } else if (resultado >= 24.9 && resultado < 29.9) {
-            System.out.println("Usted tiene sobrepeso según su IMC: " + resultado);
+            return "Sobrepeso";
         } else {
-            System.out.println("Usted tiene obesidad según su IMC: " + resultado);
+            return "Obesidad";
         }
-        return resultado;
     }
 }
