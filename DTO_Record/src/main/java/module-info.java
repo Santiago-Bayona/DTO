b985fa2d0f107f9147d45b11db7ea167ce2e47e9
Santiago-Bayona <Cesars.bayonap@uqvirtual.edu.co>
2025-03-30 21:54:51 -0500

    Update module-info.java

--- DTO_Record/src/main/java/module-info.java
+++ DTO_Record/src/main/java/module-info.java
@@ -5,4 +5,10 @@ module co.edu.uniquindio.poo.dto_record {
 
     opens co.edu.uniquindio.poo.dto_record to javafx.fxml;
     exports co.edu.uniquindio.poo.dto_record;
+    exports co.edu.uniquindio.poo.dto_record.Modelo;
+    opens co.edu.uniquindio.poo.dto_record.Modelo to javafx.fxml;
+    exports co.edu.uniquindio.poo.dto_record.Controller;
+    opens co.edu.uniquindio.poo.dto_record.Controller to javafx.fxml;
+    opens co.edu.uniquindio.poo.dto_record.ViewController to javafx.fxml;
+    exports co.edu.uniquindio.poo.dto_record.ViewController to javafx.fxml;
 }
